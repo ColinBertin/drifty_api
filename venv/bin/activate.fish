2a3a628251b5e5462f788e642fc2935df8283f6a
Colin Bertin <colinbertin12@gmail.com>
2022-05-24 15:52:40 +0900

    init

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/abauville/Web_Dev/Apps/drifty_api/venv"
+set -gx VIRTUAL_ENV "/Users/colinbertin/code/ColinBertin/drifty_api/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
