26b7b6805d9823e4c7e260b08ada32a6f7450f8e
Colin Bertin <colinbertin12@gmail.com>
2022-05-25 13:00:14 +0900

    creating route for API

---
+++ app.py
@@ -0,0 +1,29 @@
+# from flask import Flask, request, jsonify
+
+# app = Flask(__name__)
+
+
+
+# @app.route('/')
+# def hello_world():
+#     class coords:
+#         longitude = request.args.get('longitude')
+#         latitude = request.args.get('latitude')
+#     return coords.longitude
+#     # print(longitude)
+#     # print(latitude)
+
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def hello_world():
+    longitude = request.args.get('longitude')
+    latitude = request.args.get('latitude')
+    return longitude, latitude
+    # print(longitude)
+    # print(latitude)
