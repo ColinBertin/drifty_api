c39e5e82f85a56a4c6052bab7b63870f96402a41
Arthur Bauville <a.bauville@gmail.com>
2022-05-27 12:22:32 +0900

    the api returns data

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+from helpers import get_wind_speed
 
 
 
 @app.route('/')
 def hello_world():
     try:
-        longitude = float(request.args.get('lon'))
         latitude = float(request.args.get('lat'))
+        longitude = float(request.args.get('lon'))
     except TypeError:
         return jsonify({"error": "longitude and latitude must be floating point numbers. The API can be called with e.g.: base_url/?lon=-12.0&lat=13.0"})
-    return jsonify(longitude, latitude)
+
+    #　Get data
+    u, v = get_wind_speed(latitude, longitude)       
+    return jsonify( {
+        "lon": longitude, 
+        "lat": latitude, 
+        "wind_u": u[0,0],
+        "wind_v": v[0,0]
+    } )
---
+++ helpers.py
@@ -0,0 +1,15 @@
+import getgfs
+
+def get_wind_speed(lat, lon):
+  f=getgfs.Forecast("0p25")
+  res1=f.get(["ugrd10m"],"20220520 06:30", f"{lat}",f"{lon}")
+  res2=f.get(["vgrd10m"],"20220520 06:30", f"{lat}",f"{lon}")
+
+  u = res1.variables["ugrd10m"].data[0]
+  v = res2.variables["vgrd10m"].data[0]
+
+  return [u, v]
+
+
+# for d in f.search("wind"):
+#   print(d)
